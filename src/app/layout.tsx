5a6b1b8882dbb120bb9c3535c90e0ccb2dd40290
WebDevianGmbH <a.sulschani@gmail.com>
2024-11-17 14:33:26 +0100

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,37 +1,21 @@
-import type { Metadata } from "next";
-import localFont from "next/font/local";
-import "./globals.css";
-import { MobileNav } from "@/components/ui/mobile-nav";
+import { Inter } from "next/font/google"
+import "./globals.css"
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+const inter = Inter({ subsets: ["latin"] })
 
-export const metadata: Metadata = {
-  title: "Ajkune Professional",
-  description: "Ihr Unvergleichliches Haarerlebnis",
-};
+export const metadata = {
+  title: 'Ajkune Professional GmbH',
+  description: 'Ihr Unvergleichliches Haarerlebnis',
+}
 
 export default function RootLayout({
   children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+}: {
+  children: React.ReactNode
+}) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-        <MobileNav />
-      </body>
+    <html lang="de">
+      <body className={inter.className}>{children}</body>
     </html>
-  );
+  )
 }
