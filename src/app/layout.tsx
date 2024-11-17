90eed56dbc3f59a33f395aeff6c5359b692a0b80
WebDevianGmbH <a.sulschani@gmail.com>
2024-11-17 15:06:26 +0100

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google"
 import "./globals.css"
+import { MobileNav } from "@/components/ui/mobile-nav"
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -15,7 +16,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="de">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children}
+        <MobileNav />
+      </body>
     </html>
   )
 }
